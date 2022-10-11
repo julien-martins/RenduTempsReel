da8c5444960f490ee59e0c77594618acd1c2c3ea
julien-martins <39356114+julien-martins@users.noreply.github.com>
2022-10-11 18:39:06 +0200

    remove all stuff about the png loader with libpng

--- obj.h
+++ obj.h
@@ -3,16 +3,7 @@
 #include <glm/glm.hpp>
 #include <glad/glad.h>
 
-#include <png.h>
-
-#include <iostream>
-
 #include <vector>
-
-#include <cstdio>
-
-#include <fstream>
-#include <sstream>
 
 void load_obj(const char* fileName, std::vector<glm::vec4>& vertices, std::vector<glm::vec2>& uvs, std::vector<glm::vec3>& normals, std::vector<GLushort>& elements);
 
